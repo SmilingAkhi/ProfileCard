2094761ac46de0041edfccd11e2e28db741e6eeb
Abdulrahman <bhabdulrahaman@gmail.com>
2022-05-22 09:16:08 +0100

    fixed the bug

--- profile.js
+++ profile.js
@@ -4,12 +4,14 @@ const addBtn = document.querySelector(".btn");
 const skillBox = document.querySelector(".skills-container");
 const input = document.querySelector(".inp");
 
+input.focus();
+
 document.addEventListener("keydown", function (e) {
   console.log(e.key);
 
   if (e.key === "Enter") {
     inputValue = input.value;
-
+    input.value = " ";
     const firstPara = document.createElement("div");
 
     firstPara.textContent = inputValue;
@@ -20,21 +22,22 @@ document.addEventListener("keydown", function (e) {
 
     skillBox.appendChild(firstPara);
   }
+});
+
+//   alert(inputValue);
+
+addBtn.addEventListener("click", () => {
+  inputValue = input.value;
+
+  const firstPara = document.createElement("div");
+
+  firstPara.textContent = inputValue;
+  firstPara.setAttribute(
+    "style",
+    " width:auto; height:25px; border-radius:5px;text-align:center; justify-content:center; align-items:center; font-size:18px; font-family:monospace; color:maroon; background-color:rgb(35, 37, 46);color:white; "
+  );
+
+  skillBox.appendChild(firstPara);
+
   //   alert(inputValue);
-
-  addBtn.addEventListener("click", () => {
-    inputValue = input.value;
-
-    const firstPara = document.createElement("div");
-
-    firstPara.textContent = inputValue;
-    firstPara.setAttribute(
-      "style",
-      " width:auto; height:25px; border-radius:5px;text-align:center; justify-content:center; align-items:center; font-size:18px; font-family:monospace; color:maroon; background-color:rgb(35, 37, 46);color:white; "
-    );
-
-    skillBox.appendChild(firstPara);
-
-    //   alert(inputValue);
-  });
 });
